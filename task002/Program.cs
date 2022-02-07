87a035ffdb2f4cb08708be073772031f5e6124b2
«roma <teterdynko_roman@mail.ru>
2022-02-07 10:24:56 +0300

    task002 решена

---
+++ task002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Найти максимальное из трех чисел
+
+
+// Через рандом
+
+// int numA = new Random().Next(1,100);
+// Console.WriteLine($"numA: {numA}");
+// int numB = new Random().Next(1,100);
+// Console.WriteLine($"numA: {numB}");
+// int numC = new Random().Next(1,100);
+// Console.WriteLine($"numA: {numC}");
+// int max=numA;
+// if(numB>max) max=numB;
+// if(numC>max) max=numC;
+// Console.WriteLine();
+// Console.WriteLine($"max = {max}");
+
+
+// Через ввод через консоль
+
+Console.WriteLine("Введите целое число A");
+string numAStr = Console.ReadLine();
+int numA=int.Parse (numAStr);
+Console.WriteLine("Введите целое число B");
+string numBStr = Console.ReadLine();
+int numB=int.Parse (numBStr);
+Console.WriteLine("Введите целое число C");
+string numCStr = Console.ReadLine();
+int numC=int.Parse (numCStr);
+int max=numA;
+if(numB>max) max=numB;
+if(numC>max) max=numC;
+Console.WriteLine();
+Console.WriteLine($"max = {max}");
