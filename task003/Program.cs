93b1eef0bc022fca8ff4c6a8a265fd75304a277e
«roma <teterdynko_roman@mail.ru>
2022-02-07 10:56:02 +0300

    task003 решена

---
+++ task003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Выяснить является ли число чётным
+
+
+// Через ввод с консоли
+
+Console.WriteLine("Введите целое число");
+string numStr = Console.ReadLine();
+int num = int.Parse(numStr);
+Console.WriteLine();
+if (num % 2 == 0) Console.WriteLine($"Число {num} четное");
+else Console.WriteLine($"Число {num} нечетное");
+
+
+// Через рандом
+
+// int numb = new Random().Next(1, 99);
+// Console.WriteLine($"Случайное целое число {numb}");
+// Console.WriteLine();
+// if (numb % 2 == 0) Console.WriteLine($"Число {numb} четное");
+// else Console.WriteLine($"Число {numb} нечетное");
