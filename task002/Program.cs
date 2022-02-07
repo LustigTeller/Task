469a63203f0881d9d1b4356ea761ed1e4c49aaca
«roma <teterdynko_roman@mail.ru>
2022-02-07 11:02:40 +0300

    task002 mod

--- task002/Program.cs
+++ task002/Program.cs
@@ -3,15 +3,15 @@
 
 // Через рандом
 
-// int numA = new Random().Next(1,100);
+// int numA = new Random().Next(1, 100);
 // Console.WriteLine($"numA: {numA}");
-// int numB = new Random().Next(1,100);
+// int numB = new Random().Next(1, 100);
 // Console.WriteLine($"numA: {numB}");
-// int numC = new Random().Next(1,100);
+// int numC = new Random().Next(1, 100);
 // Console.WriteLine($"numA: {numC}");
-// int max=numA;
-// if(numB>max) max=numB;
-// if(numC>max) max=numC;
+// int max = numA;
+// if (numB > max) max = numB;
+// if (numC > max) max = numC;
 // Console.WriteLine();
 // Console.WriteLine($"max = {max}");
 
@@ -20,15 +20,15 @@
 
 Console.WriteLine("Введите целое число A");
 string numAStr = Console.ReadLine();
-int numA=int.Parse (numAStr);
+int numA = int.Parse(numAStr);
 Console.WriteLine("Введите целое число B");
 string numBStr = Console.ReadLine();
-int numB=int.Parse (numBStr);
+int numB = int.Parse(numBStr);
 Console.WriteLine("Введите целое число C");
 string numCStr = Console.ReadLine();
-int numC=int.Parse (numCStr);
-int max=numA;
-if(numB>max) max=numB;
-if(numC>max) max=numC;
+int numC = int.Parse(numCStr);
+int max = numA;
+if (numB > max) max = numB;
+if (numC > max) max = numC;
 Console.WriteLine();
 Console.WriteLine($"max = {max}");
