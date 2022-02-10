f808fe35fc8ddac3dba60a2a7861ed3dbb9aa5fd
«roma <teterdynko_roman@mail.ru>
2022-02-10 19:31:32 +0300

    task007 решена

---
+++ task007/Program.cs
@@ -0,0 +1,15 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+int number = new Random().Next(1, 333);
+int n = 0;
+if (number <= 100)
+{
+    Console.WriteLine($"Это число: {number}");
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    n = number % 10;
+    Console.WriteLine($"Число:{number}");
+    Console.WriteLine($"Третья чифра числа:{n}");
+}
