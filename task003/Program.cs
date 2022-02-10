d51c639eb01dbc622a5b913273e627b8cf592e7f
«roma <teterdynko_roman@mail.ru>
2022-02-10 17:57:33 +0300

    task003 mod 2.0

--- task003/Program.cs
+++ task003/Program.cs
@@ -3,18 +3,18 @@
 
 // Через ввод с консоли
 
-Console.WriteLine("Введите целое число");
-string numStr = Console.ReadLine();
-int num = int.Parse(numStr);
-Console.WriteLine();
-if (num % 2 == 0) Console.WriteLine($"Число {num} четное");
-else Console.WriteLine($"Число {num} нечетное");
-
-
-// Через рандом
-
-// int numb = new Random().Next(1, 99);
-// Console.WriteLine($"Случайное целое число {numb}");
+// Console.WriteLine("Введите целое число");
+// string numStr = Console.ReadLine();
+// int num = int.Parse(numStr);
 // Console.WriteLine();
-// if (numb % 2 == 0) Console.WriteLine($"Число {numb} четное");
-// else Console.WriteLine($"Число {numb} нечетное");
+// if (num % 2 == 0) Console.WriteLine($"Число {num} четное");
+// else Console.WriteLine($"Число {num} нечетное");
+
+
+Через рандом
+
+int numb = new Random().Next(1, 99);
+Console.WriteLine($"Случайное целое число {numb}");
+Console.WriteLine();
+if (numb % 2 == 0) Console.WriteLine($"Число {numb} четное");
+else Console.WriteLine($"Число {numb} нечетное");
