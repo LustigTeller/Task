f7df34ed95bb23b66282dee1bdfdd0dd992f51ff
«roma <teterdynko_roman@mail.ru>
2022-02-10 18:55:50 +0300

    task005 решена

---
+++ task005/Program.cs
@@ -0,0 +1,12 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+//рандом
+int number = new Random().Next(100, 999);
+int firtNumber;
+int secondNumber;
+Console.WriteLine($"Случайное число: {number}");
+firtNumber = number / 10;
+secondNumber = firtNumber % 10;
+Console.WriteLine($"Вторая цифра трехзначного числа:{secondNumber}");
+// // либо в одним действием int n (новая переменная) = number / 10 % 10;
+
