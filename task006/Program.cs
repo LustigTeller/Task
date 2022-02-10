9af5f0add7056f15250ecbd5d8294bf9f615e9dc
«roma <teterdynko_roman@mail.ru>
2022-02-10 19:10:07 +0300

    task006 решена

---
+++ task006/Program.cs
@@ -0,0 +1,9 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 999);
+int firtNumber = 0;
+int secondNumber = 0;
+Console.WriteLine($"Случайное число: {number}");
+firtNumber = number / 100;
+secondNumber = number % 10;
+Console.WriteLine($"Удалили вторую цифру трехзначного числа:{firtNumber}{secondNumber}");
