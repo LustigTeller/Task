9d589f36ae8abeb721b088eb01a3e4bd15dbbe0d
«roma <teterdynko_roman@mail.ru>
2022-02-17 19:20:07 +0300

    task009 решена

---
+++ task009/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 18: Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool x = true;
+bool y = true;
+if ((!(x || y)) == (!x && !y))
+    Console.WriteLine("выражение истино");
+else
+    Console.WriteLine("выражение ложно");
+
+x = false;
+y = true;
+if ((!(x || y)) == (!x && !y))
+    Console.WriteLine("выражение истино");
+else
+    Console.WriteLine("выражение ложно");
+
+x = true;
+y = false;
+if ((!(x || y)) == (!x && !y))
+    Console.WriteLine("выражение истино");
+else
+    Console.WriteLine("выражение ложно");
+
+x = false;
+y = false;
+if ((!(x || y)) == (!x && !y))
+    Console.WriteLine("выражение истино");
+else
+    Console.WriteLine("выражение ложно");
+
+// (мысли)
+// bool x=true;  
+// bool y=true;
+// bool valueOne = !(x || y);
+// bool valueTwo = !x && !y;
+// if(valueOne == valueTwo)
+//     Console.WriteLine("выражение истино");
+// else
+//     Console.WriteLine("выражение ложно");  
+
+// x=false;
+// y=true;
+// if(valueOne == valueTwo)
+//     Console.WriteLine("выражение истино");
+// else
+//     Console.WriteLine("выражение ложно");  
+
+// x=true;
+// y=false;
+// if(valueOne == valueTwo)
+//     Console.WriteLine("выражение истино");
+// else
+//     Console.WriteLine("выражение ложно");  
+
+// x=false;
+// y=false;
+// if(valueOne == valueTwo)
+//     Console.WriteLine("выражение истино");
+// else
+//     Console.WriteLine("выражение ложно");  
