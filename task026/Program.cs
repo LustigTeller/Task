be08b1427d5ca40ea7599297ccf01c9962810315
«roma <teterdynko_roman@mail.ru>
2022-02-20 22:06:04 +0300

    task026 решена

---
+++ task026/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26. Возведите число А в натуральную степень B используя цикл
+/*
+1. Задать метод для ввода с консоли двух чисел
+2. Считать два числа
+3. Задать цикл для возведения в степень
+*/
+
+int GetIntFromConsole (string message)    // метод на введение с консоли чисел
+{
+    Console.WriteLine(message);
+    string numberStr= Console.ReadLine();
+    int number = int.Parse(numberStr);
+    return number;
+}
+
+int numberA = GetIntFromConsole("Введите число A: ");
+int numberB = GetIntFromConsole("Введите число B: ");
+int result = 1;
+for(int i=1; i<=numberB; i++)
+{
+    result = result*numberA;
+}
+Console.WriteLine($"{numberA} в степени {numberB} = {result}");
