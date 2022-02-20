7ef00f2ef3e675db711ba18c88b1b97dc0ffe26f
«roma <teterdynko_roman@mail.ru>
2022-02-21 01:05:32 +0300

    task030 решена

---
+++ task030/Program.cs
@@ -0,0 +1,39 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+/*
+-   ввод с кансоли числа
+-   считать его + проверка
+-   определить, число с четной цифрой на конце
+-   создать цикл для вычисления кубов данных чисел
+*/
+
+int GetIntFromConsole(string message)
+{
+    int number = 0;
+    bool IsError = true;
+    while (IsError)
+    {
+        Console.WriteLine(message);
+        string numberStr = Console.ReadLine();
+        if (int.TryParse(numberStr, out number)) IsError = number <= 0;
+    }
+    return number;
+}
+int number = GetIntFromConsole("Введите число: ");
+if (number % 2 == 0)
+{
+    for (int i = 2; i <= number; i = i + 2)
+    {
+        int value = i * i * i;
+        Console.Write($"{value}; ");
+    }
+}
+else
+{
+    number = number - 1;
+    for (int i = 2; i <= number; i = i + 2)
+    {
+        int value = i * i * i;
+        Console.Write($"{value}; ");
+    }
+}
