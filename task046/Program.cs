2251447f0fcae566317e91a998f1aa7599f15b44
«roma <teterdynko_roman@mail.ru>
2022-02-28 01:25:39 +0300

    task046 mod 2.0 считаю, что не решил

--- task046/Program.cs
+++ task046/Program.cs
@@ -16,7 +16,7 @@ var data = squareCoordinates.Split(" ")
                 .Select(e => (x: double.Parse(e[0]), y: double.Parse(e[1])))
                 .Select(point => (point.x / 2, point.y / 2))
                 .ToArray();
-for(int i = 0; i < data.Length; i++)
+for (int i = 0; i < data.Length; i++)
 {
     Console.WriteLine(data[i]);
 }
